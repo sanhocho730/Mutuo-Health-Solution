0affe01926b78fe1a58b7e45c8e8e9d5007c781c
Mike Wang <41095687+darkness1418@users.noreply.github.com>
2024-02-20 00:01:18 -0800

    Mike extract questions gpt4 (#5)
    * Specify python verison in README.md
    
    Signed-off-by: Mike Wang <ww1284@nyu.edu>
    
    * Filter and extract the content from the result
    
    Signed-off-by: Mike Wang <ww1284@nyu.edu>
    
    * Add base64 encode for local image
    
    Signed-off-by: Mike Wang <ww1284@nyu.edu>
    
    * Update .gitignore to ignore .DS_Store
    
    Signed-off-by: Mike Wang <ww1284@nyu.edu>
    
    ---------
    
    Signed-off-by: Mike Wang <ww1284@nyu.edu>

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Getting Started
 
 1. Clone the repo
-2. Create a new python virtual env and activate it
+2. Create a new python virtual env (python version 3.11.3) and activate it
 3. Run `pip install -r requirements.txt`
 4. Run `pre-commit install`
 5. Add shared .env variables to .env file
--- vision.py
+++ vision.py
@@ -1,12 +1,18 @@
 from openai import AzureOpenAI
 from dotenv import load_dotenv
+import json
+import pickle
 import os
+from encode import local_image_to_data_url
 
 load_dotenv(override=True)
 
 AZURE_OPENAI_GPT4_DEPLOYMENT = os.getenv('AZURE_OPENAI_GPT4_DEPLOYMENT')
 
 azure_openai_client = AzureOpenAI()
+
+image_path = '/Users/mikewang/Desktop/Screenshot 2024-02-15 at 10.19.32 PM.png'
+data_url = local_image_to_data_url(image_path)
 
 kwargs = {
     'model': AZURE_OPENAI_GPT4_DEPLOYMENT,
@@ -16,18 +22,40 @@ kwargs = {
         {
             'role': 'user',
             'content': [
-                {'type': 'text', 'text': 'Describe this picture:'},
+                {'type': 'text', 'text': 'Extract the all text from the picture:'},
                 {
                     'type': 'image_url',
-                    'image_url': {
-                        'url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg'
-                    },
+                    'image_url': {'url': data_url},
                 },
             ],
         }
     ],
 }
 
-response = azure_openai_client.chat.completions.create(**kwargs)
+PICKLED_RESPONSE_FILE = 'pickled_response.bin'
 
-print(response)
+try:
+    with open(PICKLED_RESPONSE_FILE, 'rb') as fin:
+        response = pickle.load(fin)
+        # print(response)
+except FileNotFoundError:
+    response = azure_openai_client.chat.completions.create(**kwargs)
+    with open(PICKLED_RESPONSE_FILE, 'wb') as fout:
+        pickle.dump(response, fout)
+
+# x = response.choices[0]
+# print(x.content_filter_results)
+
+# print(json.dumps(json.loads(response.model_dump_json()), indent=4))
+
+response_json = json.loads(response.model_dump_json())
+
+# print(type(response.choices[0].message.content))
+# print(response.choices[0].message.content)
+
+print(type(response_json['choices'][0]['message']['content']))
+print(response_json['choices'][0]['message']['content'])
+
+
+# https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-madison-the-nature-boardwalk.jpg/2560px-Gfp-wisconsin-madison-the-nature-boardwalk.jpg
+# https://www.bths.edu/ourpages/auto/2022/5/12/37181053/Medical%20Example.jpg?rnd=1652382511755
